f15978f91a2b6bed599d68dc9beede8ed15dcaeb
NSrilalitha <nittala.srilalitha@gmail.com>
2020-04-14 12:41:54 +0530

    added crossorigin annotation to run app using docker

--- src/main/java/com/sample/web/app/controller/HomeController.java
+++ src/main/java/com/sample/web/app/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:8080", "http://bpvog76086dns1.eastus2.cloudapp.azure.com:8080", "http://172.18.0.1:8080"})
 public class HomeController {
 
 	@RequestMapping(value="/greet", method= RequestMethod.GET)
