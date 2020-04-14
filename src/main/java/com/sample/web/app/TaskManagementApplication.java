c6b5de4c6324fa9fd35e7e958affa7b5329531e1
NSrilalitha <nittala.srilalitha@gmail.com>
2020-04-14 14:38:40 +0530

    Added import statement for component scan

--- src/main/java/com/sample/web/app/TaskManagementApplication.java
+++ src/main/java/com/sample/web/app/TaskManagementApplication.java
@@ -3,6 +3,7 @@ package com.sample.web.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.sample.web.app"})
