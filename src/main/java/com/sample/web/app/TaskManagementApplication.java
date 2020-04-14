1ad68e5572ee60363608409e34b6d9b5052cb0b6
NSrilalitha <nittala.srilalitha@gmail.com>
2020-04-14 12:18:18 +0530

    Added annotations to run application with docker

--- src/main/java/com/sample/web/app/TaskManagementApplication.java
+++ src/main/java/com/sample/web/app/TaskManagementApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@EnableWebMvc
+@Configuration
+@ComponentScan
 @EnableTransactionManagement
 public class TaskManagementApplication {
 
