e79b71c96546a94e4c2533a38bba18995c27464b
NSrilalitha <nittala.srilalitha@gmail.com>
2020-04-14 14:23:48 +0530

    added base packages for component scan

--- src/main/java/com/sample/web/app/TaskManagementApplication.java
+++ src/main/java/com/sample/web/app/TaskManagementApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.sample.web.app"})
+@ComponentScan(basePackages={"com.sample.web.app","com.sample.web.app.controller","com.sample.web.app.model","com.sample.web.app.service","com.sample.web.app.repository"})
 @EnableWebMvc
 @EnableTransactionManagement
 public class TaskManagementApplication {
