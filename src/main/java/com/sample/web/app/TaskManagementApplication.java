4e2141e0c4463e9406e182a351330e5fdfbc39c2
NSrilalitha <nittala.srilalitha@gmail.com>
2020-04-14 14:22:04 +0530

    Reducing annotations

--- src/main/java/com/sample/web/app/TaskManagementApplication.java
+++ src/main/java/com/sample/web/app/TaskManagementApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableAutoConfiguration
-@EnableWebMvc
-@Configuration
+@SpringBootApplication
 @ComponentScan(basePackages={"com.sample.web.app"})
+@EnableWebMvc
 @EnableTransactionManagement
 public class TaskManagementApplication {
 
